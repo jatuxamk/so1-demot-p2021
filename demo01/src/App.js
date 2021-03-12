40a655affad1fa6c6f69499c450f7f73a69229a9
Janne Turunen <janne.turunen@xamk.fi>
2021-03-12 12:23:04 +0200

    React-perusteita

---
+++ demo01/src/App.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [tervehdys, setTervehdys] = useState();
+  const [nimi, setNimi] = useState("Janne");
+
+  const sanoHeippa = () => {
+
+    setTervehdys(`Heippa, ${nimi}!`);
+
+  }
+
+  return (
+    <div>
+
+      <h1>Demo 1: React-perusteita</h1>
+
+      <h2>"Hello world!"</h2>
+
+      <input type="text" placeholder="Anna nimesi..." onChange={ (e) => { setNimi(e.target.value) } } />
+
+      <button onClick={sanoHeippa}>Sano heippa</button>
+    
+      { (tervehdys) 
+        ? <div className="viesti">
+            {tervehdys}
+          </div> 
+        : null }
+
+    </div>
+  );
+}
+
+export default App;
